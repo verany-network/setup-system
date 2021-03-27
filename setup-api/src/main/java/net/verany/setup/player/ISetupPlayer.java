f208d8bc5790b4fc815ff46005e4c9c101a08b01
tylix <bytydoo@gmail.com>
2021-03-27 23:58:44 +0100

    Update ISetupPlayer.java

--- setup-api/src/main/java/net/verany/setup/player/ISetupPlayer.java
+++ setup-api/src/main/java/net/verany/setup/player/ISetupPlayer.java
@@ -1,11 +1,14 @@
 package net.verany.setup.player;
 
+import net.verany.api.gamemode.AbstractGameMode;
 import net.verany.api.interfaces.IDefault;
 
 import java.util.UUID;
 
 public interface ISetupPlayer extends IDefault<UUID> {
 
+    AbstractGameMode getCurrentEditing();
 
+    void setCurrentEditing(AbstractGameMode gameMode);
 
 }
