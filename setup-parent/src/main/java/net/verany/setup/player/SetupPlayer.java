46a74d9c9734cf331e93ed3e2ff3cfec6a8c8be5
tylix <bytydoo@gmail.com>
2021-03-27 23:58:24 +0100

    Update SetupPlayer.java

--- setup-parent/src/main/java/net/verany/setup/player/SetupPlayer.java
+++ setup-parent/src/main/java/net/verany/setup/player/SetupPlayer.java
@@ -1,15 +1,20 @@
 package net.verany.setup.player;
 
 import lombok.Getter;
+import lombok.Setter;
+import net.verany.api.gamemode.AbstractGameMode;
 import net.verany.api.loader.database.DatabaseLoader;
 import net.verany.api.module.VeranyProject;
 
 import java.util.UUID;
 
 @Getter
+@Setter
 public class SetupPlayer extends DatabaseLoader implements ISetupPlayer {
 
     private UUID uniqueId;
+
+    private AbstractGameMode currentEditing;
 
     public SetupPlayer(VeranyProject project) {
         super(project, "players", "setup");
@@ -23,4 +28,5 @@ public class SetupPlayer extends DatabaseLoader implements ISetupPlayer {
     @Override
     public void update() {
     }
+
 }
