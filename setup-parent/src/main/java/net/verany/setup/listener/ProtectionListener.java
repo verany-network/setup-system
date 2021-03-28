8813a9e576696f5e9a5e05f35b94ef076543d614
tylix <bytydoo@gmail.com>
2021-03-28 01:32:14 +0100

    Update ProtectionListener.java

--- setup-parent/src/main/java/net/verany/setup/listener/ProtectionListener.java
+++ setup-parent/src/main/java/net/verany/setup/listener/ProtectionListener.java
@@ -3,8 +3,15 @@ package net.verany.setup.listener;
 import net.verany.api.Verany;
 import net.verany.api.event.AbstractListener;
 import net.verany.api.module.VeranyProject;
+import net.verany.setup.player.ISetupPlayer;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class ProtectionListener extends AbstractListener {
 
@@ -17,6 +24,32 @@ public class ProtectionListener extends AbstractListener {
 
         Verany.registerListener(project, BlockBreakEvent.class, event -> {
             event.setCancelled(true);
+        });
+
+        Verany.registerListener(project, EntityDamageEvent.class, event -> {
+            event.setCancelled(true);
+        });
+
+        Verany.registerListener(project, FoodLevelChangeEvent.class, event -> {
+            event.setCancelled(true);
+        });
+
+        Verany.registerListener(project, PlayerChangedWorldEvent.class, event -> {
+            Player player = event.getPlayer();
+
+            if (event.getFrom().getName().equals("world")) {
+                player.setGameMode(GameMode.CREATIVE);
+                player.setFlying(true);
+            } else {
+                if (player.getWorld().getName().equals("world")) {
+                    player.setGameMode(GameMode.ADVENTURE);
+                }
+            }
+        });
+
+        Verany.registerListener(project, PlayerMoveEvent.class, event -> {
+            if (event.getPlayer().getWorld().getName().equals("world") && event.getPlayer().getLocation().getBlockY() <= 30)
+                event.getPlayer().teleport(event.getPlayer().getWorld().getSpawnLocation());
         });
     }
 }
