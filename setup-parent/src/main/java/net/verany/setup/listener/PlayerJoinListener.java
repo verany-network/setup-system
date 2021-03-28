a0fa43bb582320c167154fdb649f495bf4eec857
tylix <bytydoo@gmail.com>
2021-03-28 01:32:17 +0100

    Update PlayerJoinListener.java

--- setup-parent/src/main/java/net/verany/setup/listener/PlayerJoinListener.java
+++ setup-parent/src/main/java/net/verany/setup/listener/PlayerJoinListener.java
@@ -5,13 +5,22 @@ import net.verany.api.event.AbstractListener;
 import net.verany.api.event.events.PlayerLoadCompleteEvent;
 import net.verany.api.gamemode.AbstractGameMode;
 import net.verany.api.gamemode.GameModeWrapper;
+import net.verany.api.hotbar.HotbarItem;
+import net.verany.api.itembuilder.ItemBuilder;
 import net.verany.api.module.VeranyProject;
 import net.verany.api.player.IPlayerInfo;
 import net.verany.setup.SetupService;
+import net.verany.setup.inventory.SetupInventory;
 import net.verany.setup.player.ISetupPlayer;
 import net.verany.setup.player.SetupPlayer;
 import net.verany.setup.scoreboard.GameScoreboard;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlayerJoinListener extends AbstractListener {
 
@@ -28,6 +37,14 @@ public class PlayerJoinListener extends AbstractListener {
 
             for (IPlayerInfo onlinePlayer : Verany.getOnlinePlayers()) {
                 onlinePlayer.sendMessage(onlinePlayer.getPrefix(SetupService.INSTANCE.getModule()) + playerInfo.getNameWithColor() + " §7has joined the server§8.");
+            }
+
+            setupPlayer.setItems();
+
+            if (!player.getWorld().getName().equals("world")) {
+                player.setGameMode(GameMode.CREATIVE);
+            } else {
+                player.setGameMode(GameMode.ADVENTURE);
             }
 
             new GameScoreboard(player);
